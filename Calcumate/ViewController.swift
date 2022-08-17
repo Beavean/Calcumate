ba437c52f1a870a67576ec031b5ab1dd6f6fa339
Anthony Petrov <beavean@gmail.com>
2022-08-17 15:44:24 +0300

    Initial Commit

---
+++ Calcumate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calcumate
+//
+//  Created by Beavean on 17.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
